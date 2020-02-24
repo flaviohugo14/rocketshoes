e9eebf06694c533b6241cfc711a2ad76f84dbfa7
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-02-24 15:03:57 -0300

    add products in cart with Redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { formatPrice } from '../../util/format';
@@ -8,7 +8,7 @@ import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends React.Component {
+class Home extends React.Component {
   // eslint-disable-next-line react/state-in-constructor
   state = {
     products: [],
@@ -25,6 +25,15 @@ export default class Home extends React.Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -36,7 +45,10 @@ export default class Home extends React.Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -49,3 +61,5 @@ export default class Home extends React.Component {
     );
   }
 }
+
+export default connect()(Home);
