70b86ea3d377c33c2c1a3a926e4cc37a7e778542
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-02-28 22:36:09 -0300

    save

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
 const enhancer =
   process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
